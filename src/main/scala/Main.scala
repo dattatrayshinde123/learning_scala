bacc137e449cb39a8cb982a29366e9c5c4cc2e1e
dattatrayshinde123 <dattatrayshinde793@gmail.com>
2024-02-09 10:04:05 +0530

    Code updated

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,20 @@
+object hello {
+  def main(args: Array[String]): Unit = {
+    var isPresent=true // variable boolen type
+    var a =5
+    var b =7
+    var c=2. +(5)
+    println("equality of a==b is : " +(a!=b))
+    println("logical or of (a||b ) = " +(a|b))
+    print(isPresent, c)
+
+    var result=add(2,5)
+    println("Addititon of x and y is : "+ result)
+  }
+
+  def add(x:Int, y:Int): Int={
+    var z=x+y
+    return z  // Not mandatory to specify return function
+  }
+}
+
